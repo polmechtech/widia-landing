60eb3754d66bb107c806878b6d8cef6ea1477865
polmechtech <ndakarton@gmail.com>
2026-08-30 17:59:54 +0200

    Provide public Allegro client ID to Widia app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    ALLEGRO_CLIENT_ID: "60f9f0c6597e4eb99ba6d9c1852a9cbc",
+  },
 };
 
 export default nextConfig;
